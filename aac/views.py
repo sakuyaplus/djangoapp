fa7572a621ade54b5e9d31514d6bb6d90d316883
sakuyaplus <2358537243@qq.com>
2021-07-15 16:26:22 +0800

    测试一下index页面

--- aac/views.py
+++ aac/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+import random
+import time
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    nowtime=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    day=int(time.strftime("%Y%m%d"))
+    random.seed(day)
+    print(nowtime)
+    randomnumber=random.randint(1,100)
+    return render(request, 'index.html', {'item': randomnumber, 'time': nowtime})
