4235bd89acde6c4f5f56080f8af595aa48b203a0
sakuyaplus <2358537243@qq.com>
2021-07-18 10:09:46 +0800

    2

--- testp/settings.py
+++ testp/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'testp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ccredb',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost'
     }
 }
 
