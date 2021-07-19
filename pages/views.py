44be129118bc426202cba15ad6aa5bbd3228ef54
sakuyaplus <2358537243@qq.com>
2021-07-19 10:23:52 +0800

    7.19

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,23 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from listings.models import Listing
+from realtors.models import Realtor
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+
+    context = {
+        'listings' : listings
+    }
+    return render(request, 'index.html' ,context)
     
 def about(request):
-    return render(request, 'about.html')
+    realtors = Realtor.objects.order_by('-hire_date')
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+    context = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors
+    }
+    return render(request, 'about.html' ,context)
