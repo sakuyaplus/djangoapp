4b24432b057e0b51537b0ff3c143c0d3f1bccb32
sakuyaplus <2358537243@qq.com>
2021-07-20 09:49:53 +0800

    7.20 检索字典

--- listings/views.py
+++ listings/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
+from listings.choices import price_choices, bedroom_choices, state_choices
 from .models import Listing
 # Create your views here.
 def index(request):
@@ -10,7 +11,8 @@ def index(request):
     paged_listings = paginator.get_page(page)
 
     context={
-        'listings' : paged_listings
+        'listings' : paged_listings,
+  
     }
     return render(request, 'listings/listings.html',context)
 
@@ -24,4 +26,9 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+    context= {
+        'state_choices' : state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+    }
+    return render(request, 'listings/search.html', context)
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,7 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from listings.choices import price_choices, bedroom_choices, state_choices
 from listings.models import Listing
 from realtors.models import Realtor
 # Create your views here.
@@ -9,8 +9,12 @@ def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
 
     context = {
-        'listings' : listings
+        'listings' : listings,
+        'state_choices' : state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
     }
+   
     return render(request, 'index.html' ,context)
     
 def about(request):
