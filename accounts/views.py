74e2ed4f87bd3724d5fa03b9546faee8bed907c3
sakuyaplus <2358537243@qq.com>
2021-07-21 09:44:24 +0800

    message error

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render,redirect
-
+from django.contrib import messages
 # Create your views here.
 def register(request):
+    if request.method == 'POST':
+        messages.error(request, "Testing error")
+        return redirect('register')
+    
     return render(request, 'accounts/register.html')
 
 def login(request):
+    if request.method == 'POST':
+        print('Submited reg')
+        return redirect('index')
+    
     return render(request, 'accounts/login.html')
 
 def logout(request):
